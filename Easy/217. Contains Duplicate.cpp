b1cf3aed9171174ef369a5854a03e7b108a8e1c9
Ritesh Singh <89647516+Electric-Shade@users.noreply.github.com>
2023-05-28 17:30:25 +0530

    Update 217. Contains Duplicate.cpp
    - added alternate solution using HashMap.
    - solution in java language.

--- Easy/217. Contains Duplicate.cpp
+++ Easy/217. Contains Duplicate.cpp
@@ -12,3 +12,23 @@ public:
         return false;
     }
 };
+
+/* JAVA Solution using hashmap
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> a = new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(!a.containsKey(nums[i]))
+            {
+                a.put(nums[i],1);
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+*/
