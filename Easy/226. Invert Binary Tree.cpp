c49ccc7f2a0f6a51ef27c85a918e458d632ef6b2
Ritesh Singh <89647516+ritzzh@users.noreply.github.com>
2024-06-27 12:25:38 +0530

    Create 226. Invert Binary Tree.cpp
    Recursion, BST

---
+++ Easy/226. Invert Binary Tree.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL)
+        {
+            return NULL;
+        }
+
+        TreeNode* temp = root->left;
+        root->left=root->right;
+        root->right = temp;
+
+
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
