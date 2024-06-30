6ddcd68df4d4a9c91138718f671e26ab6e067103
Ritesh Singh <89647516+ritzzh@users.noreply.github.com>
2024-06-30 23:44:18 +0530

    Update 2824. Count Pairs Whose Sum is Less than Target.cpp
    Optimal

--- Easy/2824. Count Pairs Whose Sum is Less than Target.cpp
+++ Easy/2824. Count Pairs Whose Sum is Less than Target.cpp
@@ -18,3 +18,25 @@ public:
         return ans;
     }
 };
+
+// optimal
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int i=0,j=nums.size()-1;
+        int ans=0;
+
+        while(i<j)
+        {
+            if(nums[i]+nums[j]<target){
+                ans += j-i;
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return ans;
+    }
+};
