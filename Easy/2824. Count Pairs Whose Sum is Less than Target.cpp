dfe624ea40b1efab9a1ec2631abd6452ee141442
Ritesh Singh <89647516+ritzzh@users.noreply.github.com>
2024-06-30 23:33:24 +0530

    Create 2824. Count Pairs Whose Sum is Less than Target.cpp
    Brute force, Two pointers

---
+++ Easy/2824. Count Pairs Whose Sum is Less than Target.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int i=0,j=nums.size();
+        int ans=0;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[i]+nums[j]<target)
+                {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
