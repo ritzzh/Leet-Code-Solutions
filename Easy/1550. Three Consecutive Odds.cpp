b5bf669ac1423923fa9fe9bf70aa953d9bb1f9b7
Ritesh Singh <89647516+ritzzh@users.noreply.github.com>
2024-07-01 11:18:06 +0530

    Create 1550. Three Consecutive Odds.cpp
    Burte Force

---
+++ Easy/1550. Three Consecutive Odds.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int n = arr.size();
+        if(n==1 || n==2) return false;
+
+        for(int i=0;i<n-2;i++)
+        {
+            if(arr[i]&1 && arr[i+1]&1 && arr[i+2]&1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
