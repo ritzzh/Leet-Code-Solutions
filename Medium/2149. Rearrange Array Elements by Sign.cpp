12e403be2529ff2d7f99f118c2677723b38e797c
Ritesh Singh <89647516+ritzzh@users.noreply.github.com>
2024-07-26 10:11:29 +0530

    Update and rename 2149. Rearrange Array Elements by Sign to 2149. Rearrange Array Elements by Sign.cpp
    update: Optimized using two pointers

--- Medium/2149. Rearrange Array Elements by Sign.cpp
+++ Medium/2149. Rearrange Array Elements by Sign.cpp
@@ -26,3 +26,24 @@ public:
         return nums;
     }
 };
+
+//Optimized using two pointers
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int p = 0;
+        int n = 1;
+        vector<int> ans(nums.size(), 0);
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                ans[p] = nums[i];
+                p++;p++;
+            } else {
+                ans[n] = nums[i];
+                n++;n++;
+            }
+        }
+        return ans;
+    }
+};
