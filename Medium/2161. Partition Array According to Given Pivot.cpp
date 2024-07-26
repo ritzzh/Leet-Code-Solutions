2a7b32d032f4273a27dc333aafb6c294e7e50e34
Ritesh Singh <89647516+ritzzh@users.noreply.github.com>
2024-07-26 11:16:07 +0530

    Update 2161. Partition Array According to Given Pivot.cpp
    Two Pointers Optimized

--- Medium/2161. Partition Array According to Given Pivot.cpp
+++ Medium/2161. Partition Array According to Given Pivot.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        int less = 0;
-        int more = 0;
-        vector<int> ans(nums.size(), 0);
-
-        for (int x : nums) {
-            if (x > pivot) {
-                more++;
-            } else {
-                less++;
-            }
-        }
         int l = 0;
-        int r = less;
+        int r = nums.size() - 1;
+        int x = 0;
+        int y = r;
+        vector<int> ans(r + 1, 0);
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] < pivot) {
-                ans[l] = nums[i];
-                l++;
-            } else if (nums[i] > pivot) {
-                ans[r] = nums[i];
-                r++;
+        while (l < nums.size()) {
+            if (nums[l] < pivot) {
+                ans[x] = nums[l];
+                x++;
             }
-        }
-        while (l < less) {
-            ans[l] = pivot;
+            if (nums[r] > pivot) {
+                ans[y] = nums[r];
+                y--;
+            }
             l++;
+            r--;
+        }
+        while (x <= y) {
+            ans[x] = pivot;
+            x++;
         }
         return ans;
     }
