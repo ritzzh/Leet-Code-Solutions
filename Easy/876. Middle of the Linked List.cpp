ca91d7513321f9469a24dcbe129e53d54925cf6b
Ritesh Singh <89647516+ritzzh@users.noreply.github.com>
2024-09-07 00:36:17 +0530

    Update 876. Middle of the Linked List.cpp
    Two Pointers

--- Easy/876. Middle of the Linked List.cpp
+++ Easy/876. Middle of the Linked List.cpp
@@ -1,40 +1,19 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
-public:
-    ListNode* middleNode(ListNode* head) {
-        if(head==nullptr)
+    public ListNode middleNode(ListNode head) {
+        ListNode ptr = head;
+        ListNode ptrn = head;
+
+        while(ptrn.next!=null)
         {
-            return head;
-        }
-        ListNode *ptr = head;
-        int count=0;
-        while(ptr!=nullptr)
-        {
-            ptr=ptr->next;
-            count++;
-        }
-        count = (count/2) + 1;
-        int scount=0;
-        ptr=head;
-        while(ptr!=nullptr)
-        {
-            if(scount==count-1)
+            ptr = ptr.next;
+            if(ptrn.next.next!=null)
             {
+                ptrn = (ptrn.next).next;
+            }
+            else{
                 return ptr;
             }
-            ptr=ptr->next;
-            scount++;
-            
         }
         return ptr;
     }
-};
+}
