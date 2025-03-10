5570c10ab78314a86f9f06bb91f570f6fc8376b4
Ritesh Singh <ritesh.singh.19a@gmail.com>
2025-03-11 00:30:05 +0530

    Update 88. Merge Sorted Array.cpp
    Java Solution

--- Easy/88. Merge Sorted Array.cpp
+++ Easy/88. Merge Sorted Array.cpp
@@ -17,3 +17,38 @@ public:
         
     }
 };
+
+
+// 11 March 2025 :: {Java Solution}
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (m == 0) {
+            for (int i = 0; i < n; i++) {
+                nums1[i] = nums2[i];
+            }
+            return;
+        }
+        if (n == 0) {
+            return;
+        }
+
+        int[] ans = new int[m + n];
+
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < m + n; i++) {
+            if (x < m && y < n) {
+                ans[i] = (nums1[x] < nums2[y]) ? nums1[x++] : nums2[y++];
+            } else if (x < m) {
+                ans[i] = nums1[x++];
+            } else {
+                ans[i] = nums2[y++];
+            }
+        }
+        for (int i = 0; i < m + n; i++) {
+            nums1[i] = ans[i];
+        }
+
+    }
+}
