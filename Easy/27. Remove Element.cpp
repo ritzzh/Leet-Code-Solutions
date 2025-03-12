8ff6297f2ffcc1b79ac1a499aea0f4bbb3d62be1
Ritesh Singh <ritesh.singh.19a@gmail.com>
2025-03-13 00:15:04 +0530

    Update 27. Remove Element.cpp
    meow meow?

--- Easy/27. Remove Element.cpp
+++ Easy/27. Remove Element.cpp
@@ -14,3 +14,36 @@ public:
         return nums.size();
     }
 };
+
+
+// Java [Beats 100%]
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int l = 0;
+        int r = nums.length - 1;
+        int size = nums.length;
+
+        if (size == 1) {
+            if (nums[0] == val) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                while (nums[r] == val && r > i) {
+                    r--;
+                }
+                int temp = nums[r];
+                nums[r] = nums[i];
+                nums[i] = temp;
+            }
+            if (nums[i] != val) {
+                l++;
+            }
+        }
+        return l;
+    }
+}
