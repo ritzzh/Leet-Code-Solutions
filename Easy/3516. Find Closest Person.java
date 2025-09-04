b7a6c3e736bc0f62828328fa4377249c133c3632
Ritesh Singh <ritesh.singh.19a@gmail.com>
2025-09-04 20:24:22 +0530

    Create 3516. Find Closest Person.java

---
+++ Easy/3516. Find Closest Person.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        return (
+            Math.abs(x-z) == Math.abs(y-z) ? 0 : Math.abs(x-z) < Math.abs(y-z) ? 1 : 2
+        );
+    }
+}
